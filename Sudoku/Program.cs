53accfc962bd4506456e0af775b4b88fbf1ff0fe
Rett Robertson <94427792+rettrobertson@users.noreply.github.com>
2023-05-31 18:55:19 -0600

    Update Program.cs

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -13,15 +13,14 @@ namespace Sudoku
                 string file = dialog.FileName;
                 (char[] chars, char[][] puzzle) = FileHandler.ReadFile(file);
                 int[][] temp = FileHandler.CharToInt(chars, puzzle);
-                // int[][] justInCase = temp;
                 NakedSingle single = new();
                 Guesser guesser = new(single);
                 // NakedDouble nakedDouble = new(single);
                 // Guesser guesser = new(nakedDouble);
                 List<(int, int)>? undos = guesser.Solve(temp);
                 // the nakedDouble was having a bug every once in a blue moon on the larger puzzles that I couldn't 
-                // figure out what was happening. This code can run and not implement nakedDouble if that bug happens,
-                // but will double the time if it needs to brute force it which could be really long.
+                // figure out what was happening. Using nakedDouble will sometimes be faster but also might be wrong on very
+                // large puzzles
                 puzzle = FileHandler.IntToChar(chars, temp);
                 FileHandler.Output(undos == null, chars, puzzle, "Output.txt");
             }
